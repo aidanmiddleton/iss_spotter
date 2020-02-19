b135c97878baf61a29f1aa211866a6ac883fd3ec
Aidan Middleton <aidanemiddleton@gmail.com>
2020-02-19 01:36:58 +0000

    added iss flyover time fetching function

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const { fetchMyIP, fetchCoordsByIp } = require('./iss');
+const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -7,5 +9,23 @@ fetchMyIP((error, ip) => {
   }
 
   console.log('success, huzzah! Your IP is:' , ip);
+});
+
+fetchCoordsByIP('162.245.144.188', (error, coords) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned Coords:' , coords);
+});
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
 });
 

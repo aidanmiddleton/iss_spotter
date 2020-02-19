dbf2e2f83636d8d870dab9c38fb06090357fb3af
Aidan Middleton <aidanemiddleton@gmail.com>
2020-02-19 02:17:53 +0000

    moved functionality to one function that chains the other 3

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const { nextISSTimesForMyLocation } = require('./iss');
 
 const printPassTimes = function(passTimes) {
   for (const pass of passTimes) {
-    
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   }
 };
 
